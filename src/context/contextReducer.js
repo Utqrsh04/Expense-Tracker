0d26a477875518a821494f8100b8e593a288ada9
Utkarsh Vishwakarma <utkarshrkt2001@gmail.com>
2022-03-31 23:49:31 +0530

    Saved data in localStorage

--- src/context/contextReducer.js
+++ src/context/contextReducer.js
@@ -4,11 +4,14 @@ const contextReducer = (state, action) => {
   switch (action.type) {
     case "DELETE_TRANSACTION":
       transactions = state.filter((t) => t.id !== action.payload);
+
+      localStorage.setItem("transactions", JSON.stringify(transactions));
       return transactions;
 
     case "ADD_TRANSACTION":
       transactions = [action.payload, ...state];
-      return transactions; 
+      localStorage.setItem("transactions", JSON.stringify(transactions));
+      return transactions;
 
     default:
       break;
